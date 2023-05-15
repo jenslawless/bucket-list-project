7c7ca4a060d55dd1e9d2b63bd819e6478bded457
Jensen Lawless <jensenlawless@gmail.com>
2023-05-15 14:09:42 -0600

    featured area changes

--- src/index.js
+++ src/index.js
@@ -10,36 +10,28 @@ fetch("http://localhost:3000/myList")
         data.forEach((listItem) => myBucketList(listItem));
     })
 
-function bucketListInspo(listItem) {
-    const imgDiv = document.createElement("div");
-    const button = document.createElement("button");
-    const initImages = document.createElement("img");
-    initImages.src = listItem.image
-
-    imgDiv.append(initImages, button);
-    // const divMain = document.querySelector(".leftBlock");
-
-    imgDiv.setAttribute("class", "button-container");
-    button.setAttribute("class", "btn1");
-
-    imgDiv.append(initImages, button);
-    // divMain.append(imgDiv);
-
-}
-
 function myBucketList(listItem) {
-    const initImages = document.createElement("img")
     const li = document.createElement("li")
     const list = document.querySelector("#list-items")
-    const div = document.querySelector(".rightBlock")
-    initImages.src = listItem.image
-    div.append(initImages)
+
     li.textContent = listItem.name
     list.append(li)
 
     li.addEventListener("click", (e) => {
-        console.log("click")
+        featuredListIem(listItem)
     })
+}
+
+
+function featuredListIem(listItem) {
+    const featImage = document.querySelector(".featuredImage")
+    const featActivity = document.querySelector(".activity")
+    const featLocation = document.querySelector(".location")
+    const featDate = document.querySelector(".dateDue")
+    featActivity.textContent = "Activity: " + listItem.name
+    featLocation.textContent = "Location: " + listItem.location
+    featDate.textContent = "Complete By: " + listItem.complete_date
+    featImage.src = listItem.image
 }
 
 function bucketListInspo(listItem) {
@@ -61,7 +53,7 @@ form.addEventListener("submit", (e) => {
         "name": document.getElementById('activity').value,
         "image": document.getElementById('image-url').value,
         "location": document.getElementById('location').value,
-        "date-completion": document.getElementById('completion-date').value,
+        "complete_date": document.getElementById('completion-date').value,
         "complete": []
     }
     fetch("http://localhost:3000/myList", {
